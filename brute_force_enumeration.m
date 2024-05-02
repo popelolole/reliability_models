32437073b1d021792985e5c519628b420681767a
elionbio <eabraham@kth.se>
2024-05-02 09:37:45 +0200

    add t

--- brute_force_enumeration.m
+++ brute_force_enumeration.m
@@ -5,7 +5,7 @@ function brute_force_enumeration()
 
     RI = [0.6,0.8,0.7,0.6,0.8,0.7,0.5,0.4,0.9,0.2,0.6,0.8,0.7,0.6,0.8,0.7,0.5,0.4,0.9,0.2,0.5,0.8];
 
-    n = 22; %22 klar på ca 50s
+    n = 15; %22 ger en väntetid på ca 50s
     k = 1500;
 
     combinations = zeros(2^n, n);
@@ -13,29 +13,35 @@ function brute_force_enumeration()
     initial_combination = zeros(1, n);
 
     brute_force_enumeration(initial_combination, 1, 1);
-
-    system_prob = 0;
-    for row = 1:2^n
-        sum = 0;
-        comb_prob = 1;
-        for col = 1:n
-            if combinations(row,col) == 1
-                sum = sum + W(col);
-            end
-        end
-        if sum >= k
+    
+    system_reliability = system_rel(combinations,W,RI)
+    
+    %calculates system reliability
+    function system_prob = system_rel(combinations,W,RI)
+        system_prob = 0;
+        for row = 1:2^n
+            sum = 0;
+            comb_prob = 1;
             for col = 1:n
                 if combinations(row,col) == 1
-                    comb_prob = comb_prob * RI(col);
-                else
-                    comb_prob = comb_prob * (1-RI(col));
+                    sum = sum + W(col);
                 end
             end
-            system_prob = system_prob + comb_prob;
+            if sum >= k
+                for col = 1:n
+                    if combinations(row,col) == 1
+                        comb_prob = comb_prob * RI(col);
+                    else
+                        comb_prob = comb_prob * (1-RI(col));
+                    end
+                end
+                system_prob = system_prob + comb_prob;
+            end
         end
+        return;
     end
-    system_reliability = system_prob
-
+    
+    %finds all possible combinations
     function brute_force_enumeration(current_combination, component_index, row_index)
         if component_index > n
             combinations(row_index, :) = current_combination;

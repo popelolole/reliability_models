a83ab53ec1be7b30e85dd38ed2d447c7a5877af1
popelolole <pellebe@kth.se>
2024-05-03 14:23:13 +0200

    add node/weight/reliability experiment and refactor

--- minimal_path_sets.m
+++ minimal_path_sets.m
@@ -1,5 +1,7 @@
 %% WORK IN PROGRESS %%
 
+clc
+
 A = {Node(2, 6, 0.8), Node(3, 4, 0.7), Node(1, 2, 0.6)};
 B = {Node(1, 20, 0.5), Node(2, 17,0.6), Node(3, 15, 0.4), Node(4, 14, 0.8), Node(5, 12, 0.5), Node(6, 10, 0.4)};
 % , Node(5, 12, 0.5), Node(6, 10, 0.4), Node(7, 7, 0.6), Node(8, 5, 0.8), Node(9, 3, 0.5), Node(10, 2, 0.3)
@@ -15,87 +17,3 @@ S = Node.sortByWeight(S);
 % r = mps(A, 3, 8);
 r = mps(S, n, k)
 %r = mps(B, 4, 30)
-
-
-
-% A: array of Node instances
-function [R] = mps(A,n,k)
-    S = find_minimal_path_sets(A,n,k);
-    
-    %%% just for display
-    for i = 1:length(S)
-        Z = S{i};
-        disp("Set {")
-        for j = 1:length(Z)
-            z = Z{j};
-            disp("x" + z.index + " - weight: " + z.weight + " - r: " + z.reliability)
-        end
-        disp("}")
-    end
-    %%%
-        
-    % At this point we have R = P(S{1} u S{2} u ... u S{n})
-    % So reliability is calculated using some method, in this case sum of
-    % disjoint products (sdp).
-
-    R = sdp_method(S);
-end
-
-function [S] = find_minimal_path_sets(A,n,k)
-    S = {};
-    for i = 1:n
-        if A{i}.weight >= k
-            S{end + 1} = A(i);
-        else
-            tot = A{i}.weight;
-            Sx = A(i);
-            for j = i+1:n
-                tot = tot + A{j}.weight;
-                Sx{end + 1} = A{j};
-                if tot >= k
-                    S{end + 1} = Sx;
-                    Sx = Sx(1:end-1);
-                    tot = tot - A{j}.weight;
-                end
-            end
-        end
-    end
-end
-
-function [r] = sdp_method(S)
-     r = 0;
-    for i = 1:length(S)
-        inner_expression = {};
-        for j = 1:i - 1
-            inner_expression = Node.union(inner_expression, ...
-                Node.diff(S{j}, S{i}));
-        end
-        r = sym(r + prob(S{i}) * (probC(inner_expression)));
-    end
-    r = vpa(r);
-end
-
-function result = prob(s)
-    result = 1;
-    for i = 1:length(s)
-        result = sym(result * s{i}.reliability);
-    end
-end
-
-function result = probC(s)
-    result = 1;
-    for i = 1:length(s)
-        result = sym(result * (1 - s{i}.reliability));
-    end
-    
-    %result = 1;
-    %for i = 1:length(s)
-    %    S = s{i}
-    %    p = 1;
-    %    for j = 1:length(S)
-    %        S(j)
-    %        p = p * S(j).reliability;
-    %    end
-    %    result = result * (1 - p);
-    %end
-end
---
+++ monte_carlo_simulation.m
@@ -0,0 +1,27 @@
+function result = monte_carlo_simulation(n,k,A,iterations)
+    sys_up = 0;
+
+    % Put weights and RIs in arrays for calculations in simulation loop, 
+    % because it significantly(!) optimizes time complexity. 
+    w = [];
+    r = [];
+    for i = 1:length(A)
+        w(end + 1) = A{i}.weight;
+        r(end + 1) = A{i}.reliability;
+    end
+
+    for i = 1:iterations
+        sum = 0;
+        for j = 1:n
+            rand_num = rand();
+            if rand_num <= r(j)
+                sum = sum + w(j);
+            end
+        end
+        if sum >= k
+            sys_up = sys_up + 1;
+        end
+    end
+    disp(sys_up)
+    result = vpa(sys_up/iterations);
+end
